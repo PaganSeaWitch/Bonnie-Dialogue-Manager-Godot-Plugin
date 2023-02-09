1c45fae36645bbe928896f74da516e803ddbaeec
palmerjw <palmerjw@iu.edu>
2023-02-09 13:48:44 -0600

    Improve TokenWalker Readability

--- addons/clyde/parser/TokenWalker.gd
+++ addons/clyde/parser/TokenWalker.gd
@@ -1,50 +1,53 @@
-extends Reference
-
-const Lexer = preload('./Lexer.gd')
-
-var _tokens
-var current_token;
-var _lookadhed_tokens = [];
+class_name TokenWalker
+extends RefCounted
 
 
-func set_tokens(tokens):
-	_tokens = tokens
+var lexer : Lexer
+var current_token : Token;
+var _lookadhedlexer : Array[Token]= [];
+var _is_multiline_enabled : bool = true
 
 
-func consume(expected = null):
-	if !_lookadhed_tokens.size():
-		_lookadhed_tokens.push_back(_tokens.next())
+func setLexer(l : Lexer) -> void:
+	lexer = l
 
-	var lookahead = _lookadhed_tokens.pop_front()
 
-	if expected != null and (not lookahead or not expected.has(lookahead.token)):
+func consume(expected : Array[String] = [] ) -> Token:
+	if !_lookadhedlexer.size():
+		_lookadhedlexer.push_back(lexer.next())
+
+	var lookahead : Token = _lookadhedlexer.pop_front()
+
+	if expected.is_empty() == false && (!lookahead || !expected.has(lookahead.token)):
 		_wrong_token_error(lookahead, expected)
 
 	current_token = lookahead;
 	return current_token
 
 
-func peek(expected = null, offset = 0):
-	while _lookadhed_tokens.size() < (offset + 1):
-		var token = _tokens.next();
+func peek(expected : Array[String] = [], offset : int = 0) -> Token:
+	while _lookadhedlexer.size() < (offset + 1):
+		var token : Token = lexer.next();
 		if token:
-			_lookadhed_tokens.push_back(token);
+			_lookadhedlexer.push_back(token);
 		else:
 			break
 
-	var lookahead = _lookadhed_tokens[offset] if _lookadhed_tokens.size() > offset else null
+	var lookahead : Token = _lookadhedlexer[offset] if _lookadhedlexer.size() > offset else null
 
-	if not expected || (lookahead != null and expected.has(lookahead.token)):
+	if expected.is_empty() || (lookahead != null && expected.has(lookahead.token)):
 		return lookahead
+	
+	return null
 
 
-func _wrong_token_error(token, expected):
-	var expected_hints = []
+func _wrong_token_error(token : Token, expected : Array[String]) -> void:
+	var expected_hints : Array[String]= []
 	for e in expected:
 		expected_hints.push_back(Lexer.get_token_friendly_hint(e))
 
 	assert(false,
-		"Unexpected token \"%s\" on line %s column %s. Expected %s" % [
+		"Unexpected token \"%s\" checked line %s column %s. Expected %s" % [
 			Lexer.get_token_friendly_hint(token.token),
 			token.line+1,
 			token.column+1,
