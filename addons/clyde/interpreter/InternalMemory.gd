077a82930c2f7e0ae1acddf6b11246ea96564b3e
palmerjw <palmerjw@iu.edu>
2023-02-09 13:45:54 -0600

    Create a memory object rather then a dictionary for readability

---
+++ addons/clyde/interpreter/InternalMemory.gd
@@ -0,0 +1,11 @@
+class_name InternalMemory
+extends RefCounted
+
+
+var access    : Dictionary = {}
+
+
+var	variables : Dictionary =  {}
+
+
+var	internal  : Dictionary = {}
--- addons/clyde/interpreter/MemoryInterface.gd
+++ addons/clyde/interpreter/MemoryInterface.gd
@@ -1,4 +1,4 @@
-class_name Memory
+class_name MemoryInterface
 extends RefCounted
 
 # signal to emit when a variable has changed
@@ -6,23 +6,19 @@ signal variable_changed(name, value, previous_value)
 
 const SPECIAL_VARIABLE_NAMES = [ 'OPTIONS_COUNT' ];
 
-# Memory object
-var _mem = {
-	"access": {},
-	"variables": {},
-	"internal": {}
-}
+var internalMemory : InternalMemory = InternalMemory.new()
+
 
 
 #sets the id for a block in access as a key for true
 #This will be used to check for whether it has already been accessed
 func set_as_accessed(id):
-	_mem.access[str(id)] = true
+	internalMemory.access[str(id)] = true
 
 
 #Checks whether the access dict has corrosponding id
 func was_already_accessed(id):
-	return _mem.access.has(str(id))
+	return internalMemory.access.has(str(id))
 
 
 #Gets a variable from corrosponding id
@@ -32,7 +28,7 @@ func get_variable(id, default_value = null):
 	if SPECIAL_VARIABLE_NAMES.has(id):
 		value = get_internal_variable(id, default_value);
 	else:
-		value = _mem.variables.get(id);
+		value = internalMemory.variables.get(id);
 	
 	if (value == null):
 		return default_value;
@@ -41,36 +37,32 @@ func get_variable(id, default_value = null):
 
 #sets a variable based on its id to a value
 func set_variable(id, value):
-	variable_changed.emit(id, value, _mem.variables.get(id))
-	_mem.variables[id] = value
+	variable_changed.emit(id, value, internalMemory.variables.get(id))
+	internalMemory.variables[id] = value
 	return value
 
 
 #sets a internal variable based on its id to a value
 func set_internal_variable(id, value):
-	_mem.internal[str(id)] = value
+	internalMemory.internal[str(id)] = value
 	return value
 
 
 #gets an internal value, if null return a default value
 func get_internal_variable(id, default_value):
-	return _mem.internal.get(str(id));
+	return internalMemory.internal.get(str(id));
 
 
 #Returns memory object
 func get_all():
-	return _mem
+	return internalMemory
 
 
 #Sets data as memory object
 func load_data(data):
-	_mem = data
+	internalMemory = data
 
 
 #Resets memory object
 func clear():
-	_mem = {
-		"access": {},
-		"variables": {},
-		"internal": {}
-	}
+	internalMemory = InternalMemory.new()
