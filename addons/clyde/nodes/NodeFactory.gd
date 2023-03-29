36579fefe6448ff6e18251b97b405fc0138c2592
palmerjw <palmerjw@iu.edu>
2023-03-28 22:55:36 -0500

    Allow for chaining of node creation in NodeFactory

--- addons/clyde/nodes/NodeFactory.gd
+++ addons/clyde/nodes/NodeFactory.gd
@@ -7,6 +7,7 @@ LINE, BLOCK, VARIATIONS, VARIABLE,
 LITERAL, NULL, ASSIGNMENTS, CONDITIONAL_CONTENT, 
 ACTION_CONTENT, EVENTS, EVENT, CONTENT, STRINGLITERAL, NUMBERLITERAL, BOOLEANLITERAL}
 
+
 var NodeFactoryDictionary : Dictionary = {
 	NODE_TYPES.DOCUMENT : DocumentNode,
 	NODE_TYPES.DIVERT : DivertNode,
@@ -30,6 +31,29 @@ var NodeFactoryDictionary : Dictionary = {
 	NODE_TYPES.CONTENT : ContentNode
 }
 
+var NodeFactoryDictionaryReverse : Dictionary = {
+	DocumentNode : NODE_TYPES.DOCUMENT,
+	DivertNode : NODE_TYPES.DIVERT,
+	OptionNode: NODE_TYPES.OPTION,
+	OptionsNode : NODE_TYPES.OPTIONS,
+	LineNode : NODE_TYPES.LINE,
+	BlockNode : NODE_TYPES.BLOCK,
+	VariationsNode : NODE_TYPES.VARIATIONS,
+	VariableNode: NODE_TYPES.VARIABLE,
+	StringNode : NODE_TYPES.STRINGLITERAL,
+	NumberNode : NODE_TYPES.NUMBERLITERAL,
+	BooleanNode : NODE_TYPES.BOOLEANLITERAL,
+	NullTokenNode : NODE_TYPES.NULL,
+	AssignmentsNode : NODE_TYPES.ASSIGNMENTS,
+	ExpressionNode : NODE_TYPES.EXPRESSION,
+	AssignmentNode : NODE_TYPES.ASSIGNMENT,
+	ConditionalContentNode : NODE_TYPES.CONDITIONAL_CONTENT,
+	ActionContentNode : NODE_TYPES.ACTION_CONTENT,
+	EventsNode : NODE_TYPES.EVENTS,
+	EventNode : NODE_TYPES.EVENT,
+	ContentNode : NODE_TYPES.CONTENT
+}
+
 func CreateNode(type : NODE_TYPES, args : Dictionary) -> ClydeNode:
 	var node = NodeFactoryDictionary.get(type).new();
 	
@@ -46,6 +70,33 @@ func CreateNode(type : NODE_TYPES, args : Dictionary) -> ClydeNode:
 		return NumberLiteral(node)
 	if(node is	BooleanNode):
 		return BooleanLiteral(node)
+	return node
+
+func CreateNodeTree(jsonDictionary : Dictionary) -> ClydeNode:
+	var node = NodeFactoryDictionary.get(jsonDictionary["type"] as NodeFactory.NODE_TYPES).new()
+	var properties = node.get_property_list()
+	
+	for property in node.get_property_list():
+		if(jsonDictionary.has(property.name)):
+			var currentVal = jsonDictionary.get(property.name)
+			
+			match(typeof(currentVal)):
+				
+				TYPE_ARRAY:
+					var array : Array = []
+					if(!currentVal.is_empty() && typeof(currentVal[0]) == TYPE_DICTIONARY):
+						for dic in currentVal:
+							array.append(CreateNodeTree(dic))
+						node[property.name] = array
+					else:
+						node[property.name] = currentVal
+				
+				TYPE_DICTIONARY:
+					if(currentVal.size() != 0):
+						node[property.name] = CreateNodeTree(currentVal)
+				
+				TYPE_STRING, TYPE_BOOL, TYPE_FLOAT:
+					node[property.name] = currentVal
 	return node
 
 
