b3c2d7d6724107ac5db9f5748050bcb707575ae9
palmerjw <palmerjw@iu.edu>
2023-03-28 22:54:59 -0500

    Add to document node  function to parser

--- addons/clyde/parser/Parser.gd
+++ addons/clyde/parser/Parser.gd
@@ -44,39 +44,62 @@ var NODE_TYPE_DICTIONARY : Dictionary = {
 
 func to_JSON_object(doc : ClydeNode) -> Dictionary:
 	var json_dictionary : Dictionary = {}
-	var keys = NODE_TYPE_DICTIONARY.keys()
-
+	var keys : Array = NodeFactory.new().NodeFactoryDictionaryReverse.keys()
+	if(doc == null):
+		return {}
 	for key in keys:
-		if(doc is key):
-			json_dictionary["type"] = NODE_TYPE_DICTIONARY[key]
+		if is_instance_of(doc, key):
+			var number : int = NodeFactory.new().NodeFactoryDictionaryReverse[key]
+			json_dictionary["type"] = number
 			break
+	if(!json_dictionary.has("type")):
+		return {}
 
 	for property in doc.get_property_list():
-		if(property.name == "value"):
-			pass
 		match(property.type):
-			28:
+			TYPE_ARRAY:
 				var array : Array = []
 				if(property.name != "tags" && property.name != "id_suffixes"):
 					for node in doc[property.name]:
 						array.append(to_JSON_object(node))
 					json_dictionary[property.name] = array
-				#elif(typeof(doc[property.name]) == 28):
-				#		for node in doc[property.name]:
-				#			array.append(to_JSON_object(node))
-				#			json_dictionary[property.name] = array
 				else:
 					json_dictionary[property.name] = doc[property.name]
-			24:
+			TYPE_OBJECT:
 				if(property.name != "script"):
 					json_dictionary[property.name] = to_JSON_object(doc[property.name])
-			4, 1, 3:
+			TYPE_STRING, TYPE_BOOL, TYPE_FLOAT:
 				json_dictionary[property.name] = doc[property.name]
-			0:
-				if((property.name == "value"|| property.name == "variable" )&& json_dictionary["type"] == "ASSIGNMENT"):
+			TYPE_NIL:
+				if((property.name == "value"|| property.name == "variable" )&& json_dictionary["type"] == NodeFactory.NODE_TYPES.ASSIGNMENT):
 					json_dictionary[property.name] = to_JSON_object(doc[property.name])
 				elif(property.name != "RefCounted" && !property.name.ends_with(".gd")):
 					json_dictionary[property.name] = doc[property.name]
 			
 	return json_dictionary
 
+
+func to_Document_node(jsonDictionary : Dictionary) -> ClydeNode:
+	if(validate_json_object(jsonDictionary)):
+		return NodeFactory.new().CreateNodeTree(jsonDictionary);
+	return null
+
+
+func validate_json_object(jsonDictionary : Dictionary) -> bool:
+	var isNotBroken : bool = true
+	var keys  : Array = NodeFactory.new().NodeFactoryDictionary.keys()
+	
+	if(!jsonDictionary.keys().has("type") || !keys.has(jsonDictionary["type"] as NodeFactory.NODE_TYPES)):
+		return false
+	
+	for value in jsonDictionary.values():
+		match(typeof(value)):
+			TYPE_ARRAY:
+				if(!value.is_empty() && typeof(value[0]) == TYPE_DICTIONARY):
+					for dic in value:
+						isNotBroken = isNotBroken && validate_json_object(dic)
+
+			TYPE_DICTIONARY:
+				if(value.size() != 0):
+					isNotBroken = isNotBroken && validate_json_object(value)
+	return isNotBroken
