ec4054ac5c54c7398a0cd2d69c9868a10b3ac20b
palmerjw <palmerjw@iu.edu>
2023-04-02 21:04:24 -0500

    Add better to string method

--- addons/clyde/nodes/Clyde_Node.gd
+++ addons/clyde/nodes/Clyde_Node.gd
@@ -11,6 +11,28 @@ func is_node_class(className : String ) -> bool:
 	return get_node_class().to_lower() == className.to_lower()
 
 func _to_string() -> String:
-	var string = Parser.new().to_JSON_object(self, true)
-	print(string)
+	var dictionary = Parser.new().to_JSON_object(self, true)
+	print("###############################")
+	var indent = 0;
+	_to_node_string(dictionary, indent)
+	print("###############################")
 	return "" 
+	
+func _to_node_string(dictionary : Dictionary, indent : int):
+	for key in dictionary.keys():
+		if(dictionary[key] is Array && !dictionary[key].is_empty()):
+
+			if(dictionary[key][0] is Dictionary):
+				print(" ".repeat(indent) + key + " : ")
+				for dict in dictionary[key]:
+					_to_node_string(dict, indent + 1)
+			else:
+				var string = ""
+				for val in dictionary[key]:
+					string =  val +", " +  string
+				string = string.substr(0, string.length() - 2)
+				print(" ".repeat(indent) + key + " : " + string)
+
+		elif(dictionary[key] is String && !dictionary[key].is_empty()):
+			print(" ".repeat(indent) + key +" : " + dictionary[key])
+	
