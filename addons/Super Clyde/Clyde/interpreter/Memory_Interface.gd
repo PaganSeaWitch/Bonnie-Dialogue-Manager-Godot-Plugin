701986adee48a38b6356b923f957f4a5159080df
PaganSeaWitch <palmerjw@iu.edu>
2023-06-16 10:33:59 -0500

    More bug fixes

--- addons/Super Clyde/Clyde/interpreter/Memory_Interface.gd
+++ addons/Super Clyde/Clyde/interpreter/Memory_Interface.gd
@@ -13,18 +13,18 @@ signal variable_changed(name, value, previous_value)
 
 const SPECIAL_VARIABLE_NAMES : Array[String]= [ 'OPTIONS_COUNT' ];
 
-var internalMemory : ClydeInternalMemory = ClydeInternalMemory.new()
+var clyde_internal_memory : ClydeInternalMemory = ClydeInternalMemory.new()
 
 
 # Sets the id for a block in access as a key for true
 # This will be used to check for whether it has already been accessed
 func set_as_accessed(id) -> void:
-	internalMemory.access[str(id)] = true
+	clyde_internal_memory.access[str(id)] = true
 
 
 # Checks whether the access dict has corrosponding id
 func was_already_accessed(id) -> bool:
-	return internalMemory.access.has(str(id))
+	return clyde_internal_memory.access.has(str(id))
 
 
 # Gets a variable from corrosponding id
@@ -34,7 +34,7 @@ func get_variable(id, default_value = null):
 	if SPECIAL_VARIABLE_NAMES.has(id):
 		value = get_internal_variable(id, default_value);
 	else:
-		value = internalMemory.variables.get(id);
+		value = clyde_internal_memory.variables.get(id);
 	
 	if (value == null):
 		return default_value;
@@ -43,33 +43,33 @@ func get_variable(id, default_value = null):
 
 # Sets a variable based on its id to a value
 func set_variable(id, value):
-	variable_changed.emit(id, value, internalMemory.variables.get(id))
-	internalMemory.variables[id] = value
+	variable_changed.emit(id, value, clyde_internal_memory.variables.get(id))
+	clyde_internal_memory.variables[id] = value
 	return value
 
 
 # Sets a internal variable based on its id to a value
 func set_internal_variable(id, value):
-	internalMemory.internal[str(id)] = value
+	clyde_internal_memory.internal[str(id)] = value
 	return value
 
 
 # Gets an internal value, if null return a default value
 func get_internal_variable(id, default_value):
-	return (internalMemory.internal.get(str(id)) 
-		if internalMemory.internal.get(str(id)) != null else default_value)
+	return (clyde_internal_memory.internal.get(str(id)) 
+		if clyde_internal_memory.internal.get(str(id)) != null else default_value)
 
 
 # Returns memory object
 func get_all() -> ClydeInternalMemory:
-	return internalMemory
+	return clyde_internal_memory
 
 
 # Sets data as memory object
 func load_data(data : ClydeInternalMemory) -> void:
-	internalMemory = data
+	clyde_internal_memory = data
 
 
 # Resets memory object
 func clear() -> void:
-	internalMemory = ClydeInternalMemory.new()
+	clyde_internal_memory = ClydeInternalMemory.new()
